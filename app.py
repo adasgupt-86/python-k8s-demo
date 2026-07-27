b8a518f7ea3cc76362dd33d4f6346a42ac7759b7
Abhishek Dasgupta <abhishek.dasgupta11@gmail.com>
2026-07-28 03:32:30 +0530

    Add Flask application and Dockerfile

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello Jenkins")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Jenkins Shared Library Lab!"
+
+@app.route("/health")
+def health():
+    return {"status": "UP"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
